717b2fcb41e91387912fb4c277f9f05ce93ac51d
throstur78 <42745670+throstur78@users.noreply.github.com>
2018-11-04 22:08:48 +0000

    Vann í script

--- scripts.js
+++ scripts.js
@@ -4,52 +4,99 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('.form');
   const items = document.querySelector('.items');
   
+  
   text.init(form, items);
 });
 
 const text = (() => {
   let items;
   let input;
+  let text;
   
   
   function init(_form, _items) {
-    items = _items;
+
+    items=_items;
 
     //Fyrir formið
     input = _form.querySelector('.form__input');
     _form.addEventListener('submit', formHandler);
     
     //Fyrir items
-    _items.addEventListener('change',finish);
-    //_items.addEventListener('input',edit);
-    //buttonDestroy.addEventListener('click', deleteItem);
+    _items.addEventListener('click',itemsHandler);
+    text = _items.querySelector('.item__text');
   }
 
+  function itemsHandler(e) {
+    e.preventDefault();
+    event.stopPropagation();
+
+    if(e.target.className==="item__checkbox")
+    {  
+      finish(e);
+    }
+    else if(e.target.className==="item__text")
+    {
+      edit(e);
+    }
+    else if(e.target.className==="item__button")
+    {
+      deleteItem(e);
+    }
+  }
+
+  // event handler fyrir það að haka við færslu
+  function finish(e) {
+    e.preventDefault();
+
+    const el = e.target.parentNode;
+    el.classList.toggle('item--done');   
+  }
+
+  // event handler fyrir að það að eyða færslu
   function deleteItem(e) {
     e.preventDefault();
+    console.log(e);
     const el = e.target.parentNode;
     const el1 = el.parentNode;
-    el1.removeChild(el);
+    el1.removeChild(el); 
   }
 
   // event handler fyrir það að breyta færslu
   function edit(e) {
-    console.log('erum að edita!!!');
+    console.log('Ættum að vera að edita en ekkert gengur!!');
   }
 
   // event handler fyrir það að klára að breyta færslu
   function commit(e) {
     
   }
+  
+  function formHandler(e) {
+    e.preventDefault();
+    
+    let texti = input.value;
+    if(texti.trim().length===0)
+    {
+      input.value = "";  
+    }
+    else
+    {
+      add(texti);
+      input.value = "";
+    }
+  }
 
+  
   // fall sem sér um að bæta við nýju item
   function add(value) {
-    console.log('add virkar');
+    
     const liElement = document.createElement('li');
     liElement.classList.add('item');
 
     const checkboxElement = document.createElement('input');
     checkboxElement.classList.add('item__checkbox');
+    checkboxElement.setAttribute('type','checkbox');
 
     const textElement = document.createElement('span');
     textElement.classList.add('item__text');
@@ -57,37 +104,13 @@ const text = (() => {
 
     const buttonElement = document.createElement('button');
     buttonElement.classList.add('item__button');
+    buttonElement.appendChild(document.createTextNode("Eyða"));
 
     liElement.appendChild(checkboxElement);
     liElement.appendChild(textElement);
     liElement.appendChild(buttonElement);
-    
 
-  }
-  
-  function formHandler(e) {
-    e.preventDefault();
-  
-    let texti = input.value;
-    add(texti);
-    input.value = "";
-  }
-
-  // event handler fyrir það að klára færslu
-  function finish(e) {
-    e.preventDefault();
-    const el = event.target.parentNode;
-    el.classList.toggle('item--done');   
-  }
-
-  // hjálparfall til að útbúa element
-  function el(type, className, clickHandler) {
-    //const el = document.createElement(li,item,);
-      //Hvernig eru þessi börn bætt við??? 
-      //<input type="checkbox" checked class="item__checkbox">
-      //<span class="item__text">Sækja verkefni</span>
-      //<button class="item__button">Eyða</button>
-
+    items.appendChild(liElement);   
   }
 
   return {
